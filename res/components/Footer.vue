7ff159cff572262f20a047a463eba12ba13057ca
luliangshu <lu3501037@gmail.com>
2019-06-06 14:43:52 +0800

    modify

--- res/components/Footer.vue
+++ res/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="common_footer">
     <h5>上海都设营造建筑设计事务所有限公司</h5>
     <h6>DuShe Architectural Design Co.Ltd.,Shanghai</h6>
-    <h6>沪ICP备14054063号</h6>
+    <h6>沪ICP备18038338号-1</h6>
   </div>
 </template>
 
