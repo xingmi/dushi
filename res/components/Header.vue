32d243d1fae82650ef62eb9741f421439eac6ced
luliangshu <lu3501037@gmail.com>
2019-06-06 14:02:36 +0800

    modify

--- res/components/Header.vue
+++ res/components/Header.vue
@@ -11,50 +11,50 @@
         </ul>
         <ol class="sub_nav single sub_nav_1">
           <li>
-            <a href="idea.html?type=design&number=3" :class="{current: urlType == 'design'}">一体化设计</a>
-            <a href="idea.html?type=value&number=3" :class="{current: urlType == 'value'}">价值发掘者</a>
-            <a href="idea.html?type=completeness&number=3" :class="{current: urlType == 'completeness'}">高完成度</a>
+            <a href="/cn/idea.html?type=design&number=3" :class="{current: urlType == 'design'}">一体化设计</a>
+            <a href="/cn/idea.html?type=value&number=3" :class="{current: urlType == 'value'}">价值发掘者</a>
+            <a href="/cn/idea.html?type=completeness&number=3" :class="{current: urlType == 'completeness'}">高完成度</a>
           </li>
         </ol>
         <ol class="sub_nav sub_project sub_nav_2 clear_fix">
           <li class="location">
             <h4>地区</h4>
-            <a href="project.html?type=华东" :class="{current: urlType == '华东'}">华东</a>
-            <a href="project.html?type=华北" :class="{current: urlType == '华北'}">华北</a>
-            <a href="project.html?type=华中" :class="{current: urlType == '华中'}">华中</a>
-            <a href="project.html?type=华南" :class="{current: urlType == '华南'}">华南</a>
-            <a href="project.html?type=西南" :class="{current: urlType == '西南'}">西南</a>
+            <a href="/cn/project.html?type=华东" :class="{current: urlType == '华东'}">华东</a>
+            <a href="/cn/project.html?type=华北" :class="{current: urlType == '华北'}">华北</a>
+            <a href="/cn/project.html?type=华中" :class="{current: urlType == '华中'}">华中</a>
+            <a href="/cn/project.html?type=华南" :class="{current: urlType == '华南'}">华南</a>
+            <a href="/cn/project.html?type=西南" :class="{current: urlType == '西南'}">西南</a>
           </li>
           <li style="width: 220px;">
             <h4>类型</h4>
-            <a href="project.html?type=商业+办公" :class="{current: urlType == '商业+办公'}">商业+办公</a>
-            <a href="project.html?type=住宅" :class="{current: urlType == '住宅'}" style="width: 50px;">住宅</a>
-            <a href="project.html?type=酒店" :class="{current: urlType == '酒店'}">酒店</a>
-            <a href="project.html?type=旅游小镇" :class="{current: urlType == '旅游小镇'}">旅游小镇</a>
-            <a href="project.html?type=文化" :class="{current: urlType == '文化'}" style="width: 50px;">文化</a>
-            <a href="project.html?type=城市更新" :class="{current: urlType == '城市更新'}">城市更新</a>
+            <a href="/cn/project.html?type=商业+办公" :class="{current: urlType == '商业+办公'}">商业+办公</a>
+            <a href="/cn/project.html?type=住宅" :class="{current: urlType == '住宅'}" style="width: 50px;">住宅</a>
+            <a href="/cn/project.html?type=酒店" :class="{current: urlType == '酒店'}">酒店</a>
+            <a href="/cn/project.html?type=旅游小镇" :class="{current: urlType == '旅游小镇'}">旅游小镇</a>
+            <a href="/cn/project.html?type=文化" :class="{current: urlType == '文化'}" style="width: 50px;">文化</a>
+            <a href="/cn/project.html?type=城市更新" :class="{current: urlType == '城市更新'}">城市更新</a>
           </li>
           <li>
             <h4>服务</h4>
-            <a href="project.html?type=室内" :class="{current: urlType == '室内'}" style="width: 50px;">室内</a>
-            <a href="project.html?type=一体化设计" :class="{current: urlType == '一体化设计'}">一体化设计</a>
-            <a href="project.html?type=规划" :class="{current: urlType == '规划'}">规划</a>
-            <a href="project.html?type=建筑" :class="{current: urlType == '建筑'}" style="width: 50px;">建筑</a>
+            <a href="/cn/project.html?type=室内" :class="{current: urlType == '室内'}" style="width: 50px;">室内</a>
+            <a href="/cn/project.html?type=一体化设计" :class="{current: urlType == '一体化设计'}">一体化设计</a>
+            <a href="/cn/project.html?type=规划" :class="{current: urlType == '规划'}">规划</a>
+            <a href="/cn/project.html?type=建筑" :class="{current: urlType == '建筑'}" style="width: 50px;">建筑</a>
           </li>
         </ol>
         <ol class="sub_nav single sub_nav_3">
           <li>
-            <a href="news.html?type=news" :class="{current: urlType == 'news'}">新闻</a>
-            <a href="news.html?type=articles" :class="{current: urlType == 'articles'}">出版物/文章</a>
-            <a href="news.html?type=propaganda" :class="{current: urlType == 'propaganda'}">在网络媒体的宣传</a>
+            <a href="/cn/news.html?type=news" :class="{current: urlType == 'news'}">新闻</a>
+            <a href="/cn/news.html?type=articles" :class="{current: urlType == 'articles'}">出版物/文章</a>
+            <a href="/cn/news.html?type=propaganda" :class="{current: urlType == 'propaganda'}">在网络媒体的宣传</a>
           </li>
         </ol>
         <ol class="sub_nav single sub_nav_4">
           <li>
-            <a href="awards.html?type=award" :class="{current: urlType == 'award'}">奖项</a>
-            <a href="member.html?type=member" :class="{current: urlType == 'member'}">核心团队</a>
-            <a href="jobs.html?type=join" :class="{current: urlType == 'join'}">工作机会</a>
-            <a href="contact.html?type=contact" :class="{current: urlType == 'contact'}">联系我们</a>
+            <a href="/cn/awards.html?type=award" :class="{current: urlType == 'award'}">奖项</a>
+            <a href="/cn/member.html?type=member" :class="{current: urlType == 'member'}">核心团队</a>
+            <a href="/cn/jobs.html?type=join" :class="{current: urlType == 'join'}">工作机会</a>
+            <a href="/cn/contact.html?type=contact" :class="{current: urlType == 'contact'}">联系我们</a>
           </li>
         </ol>
     </div>
