15c5c3b1005c71a1d5aef1cd1d2ef83a15154a2d
luliangshu <lu3501037@gmail.com>
2020-01-03 21:06:31 +0800

    du

--- res/components/Footer.vue
+++ res/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="common_footer">
     <h5>上海都设营造建筑设计事务所有限公司</h5>
     <h6>DuShe Architectural Design Co.Ltd.,Shanghai</h6>
-    <h6>沪ICP备18038338号-1</h6>
+    <h6>沪ICP备<a href="http://www.beian.miit.gov.cn" style="color: #bdbdbd">18038338号-1</a></h6>
   </div>
 </template>
 
